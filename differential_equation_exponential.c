f817e3b3223a333e4961632205ce9f588e972040
Archisman Panigrahi <archisman@archisman.tk>
2019-01-06 23:34:42 +0530

    Refinements
    Included non-linear increments.
    
    Also, there was a bug of values being a bit inaccurate for too many iterations. It  was (unexpectedly) changed after using long double instead of float variables. Why, though?

--- differential_equation_exponential.c
+++ differential_equation_exponential.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-void main(int argc, char const *argv[])
+void main()
 {
-	float x,y,i,y_0,k,x_f,delta_x,iterations;
+	long double x,y,i,y_0,k,x_f,delta_x,iterations;
 	printf("Input initial value:	");
-	scanf("%f", &y_0);
+	scanf("%Lf", &y_0);
 	printf("Input rate constant:	");
-	scanf("%f", &k);
+	scanf("%Lf", &k);
 	printf("Enter value of x to calculate y for:	");
-	scanf("%f", &x_f);
+	scanf("%Lf", &x_f);
 	printf("Enter no of interations:	");
-	scanf("%f", &iterations);
+	scanf("%Lf", &iterations);
 	y = y_0;
 	delta_x = x_f/iterations;
-	for (i = 0; i < iterations; ++i)
+	for (i = 0; i < iterations; i++)
 	{
-		y = y + ((k*y)*delta_x);
-		printf("Current no of step %f and y is %f\n", i,y);
+		y = y + ((k*y)*delta_x)+(((k*k*y)*delta_x*delta_x)/2)+(((k*k*k*y)*delta_x*delta_x*delta_x)/6);
+		printf("Current no of step %Lf and y is %Lf\n", i,y);
 	}
-	printf("y is %f\n", y);
+	printf("y is %Lf at x = %Lf\n", y, x_f);
 }
